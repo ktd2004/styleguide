e14777c048b2017da7c5c75e32fbbc0107984c7b
TaeDong Kim <fly1004@gmail.com>
2012-12-04 12:21:15 +0900

    변환하는 script 파일 추가

---
+++ po/updatepo.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+
+msgmerge --update --backup=none --no-wrap --quiet --no-fuzzy-matching --sort-output cppguide_kr.po cppguide.pot
---
+++ po/updatepot.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+
+cd ..
+xml2po -e -o po/cppguide.pot cppguide.xml
+
